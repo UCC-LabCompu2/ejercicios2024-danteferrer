5f486d898aa49c423801181ad00d31eb92b16c79
danteferrer <135678008+danteferrer@users.noreply.github.com>
2024-06-22 18:39:48 -0300

    termine el ejercicio

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -65,24 +65,24 @@ function calcularSuma(){
     var num1, num2;
     num1= Number( document.getElementsByName("sum_num1")[0].value);
     num2=document.getElementsByName("sum_num2")[0].value;
-    document.getElementsByName("sum_total")[0].value=num1+ Number(num2);
+    document.getElementsByName("sum_total")[0].innerHTML=num1+ Number(num2);
 }
 function calcularResta(){
     var num1, num2;
     num1= Number ( document.getElementsByName("res_num1")[0].value);
     num2=document.getElementsByName("res_num2")[0].value;
-    document.getElementsByName("res_total")[0].value= num1 - Number(num2);
+    document.getElementsByName("res_total")[0].innerHTML= num1 - Number(num2);
 }
 function calcularMulti(){
     var num1, num2;
     num1= Number( document.getElementsByName("mul_num1")[0].value);
     num2=document.getElementsByName("mul_num2")[0].value;
-    document.getElementsByName("mul_total")[0].value=num1 * Number(num2);
+    document.getElementsByName("mul_total")[0].innerHTML=num1 * Number(num2);
 }
 function calcularDiv(){
     var num1, num2;
     num1= Number( document.getElementsByName("div_num1")[0].value);
     num2=document.getElementsByName("div_num2")[0].value;
-    document.getElementsByName("div_total")[0].value=num1 / Number(num2);
+    document.getElementsByName("div_total")[0].innerHTML=num1 / Number(num2);
 }
 
