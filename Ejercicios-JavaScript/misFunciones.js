94fdb8887661d2ff73209ae44ec5104bb4fa9cbf
danteferrer <135678008+danteferrer@users.noreply.github.com>
2024-06-22 17:37:02 -0300

    termine el ejercicio

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -30,3 +30,15 @@ function cambiarUnidades(id,value){
         document.lasUnidades.unid_metro.value=0.9144*value;
     }
 }
+function convertirGR(id){
+    var grad, rad;
+    if (id=="grados"){
+        grad=document.getElementById("grados").value;
+        rad= (grad* Math.PI)/180;
+    }else if (id=="radianes"){
+        rad=document.getElementById("radianes").value;
+        grad= (rad*180)/Math.PI;
+    }
+    document.getElementById("grados").value=grad;
+    document.getElementById("radianes").value=rad;
+}
