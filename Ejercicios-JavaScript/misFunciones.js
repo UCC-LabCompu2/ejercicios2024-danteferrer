0115c2285e2a06c0c34c47cc6504f9531a1d71fa
danteferrer <135678008+danteferrer@users.noreply.github.com>
2024-06-22 11:26:06 -0300

    termine el ejercicio

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,9 +5,28 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
-function cargaLS{
-    const distancia: string= localStorage.getItem()
-}
-function dibujarCirculoCuadrado(){
-    const canvas: HTMLElement=document.getElementById("myCanvas");
+function cambiarUnidades(id,value){
+    if(isNaN(value)){
+        alert("se ingreso un valor incorecto");
+        document.lasUnidades.unid_metro.value="";
+        document.lasUnidades.unid_pulgada.value=""
+        document.lasUnidades.unid_pie.value=""
+        document.lasUnidades.unid_yarda.value=""
+    }else if (id=="metro"){
+        document.lasUnidades.unid_pulgada.value=39.3701*value;
+        document.lasUnidades.unid_pie.value=3.28084*value;
+        document.lasUnidades.unid_yarda.value=1.09361*value;
+    }else if (id=="pulgada"){
+        document.lasUnidades.unid_metro.value=0.0254*value;
+        document.lasUnidades.unid_pie.value=0.0833333*value;
+        document.lasUnidades.unid_yarda.value=0.0277778*value;
+    }else if (id=="pie"){
+        document.lasUnidades.unid_pulgada.value=12*value;
+        document.lasUnidades.unid_metro.value=0.3048*value;
+        document.lasUnidades.unid_yarda.value=0.333333*value;
+    }else if (id=="yarda"){
+        document.lasUnidades.unid_pulgada.value=36*value;
+        document.lasUnidades.unid_pie.value=3*value;
+        document.lasUnidades.unid_metro.value=0.9144*value;
+    }
 }
